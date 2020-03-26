90e902349289467ea892a472e0cf9a2052716ac5
CELL-Company <62654950+CELL-Company@users.noreply.github.com>
2020-03-25 22:13:25 -0500

    Update comp.py

--- comp.py
+++ comp.py
@@ -1,16 +1,16 @@
+import io
 #functions taken from stackoverflow
-#ok so like, i havent touched py in a while. The comp algo has to go through 
-#many iterations before it "breaks even"
 def toBin(te):
     res = ''.join(format(ord(i), 'b').zfill(8) for i in te)
     return(res)
 def se(item, split_num):
     return [item[i:i+split_num] for i in range(0, len(item), split_num)]
 
-
-with open("file.txt","r") as file:
+charrarr = se("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./0123456789:;<=>?@ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ",1)
+with open("file.txt",encoding="cp437") as file:
     text = str(file.read())
     print(text)
+    print(len(text))
     string = str(toBin(text))
     print(string)
     print(len(string))
@@ -20,7 +20,7 @@ with open("file.txt","r") as file:
     print(len(arr))
     finarr = [];
     for i in range(1,len(arr),2):
-        if int(arr[i]) % 2 != 0:
+        if int(i) % 2 != 0:
             arr[i] = arr[i].replace("0","2").replace("1","4")
             print(arr)
     for i in range(0, len(arr)-1,2):   
@@ -51,48 +51,55 @@ with open("file.txt","r") as file:
         print(newarr)
     string = "".join(newarr)
     print(len(string))
-    
-"""
-    a = str(int(arr[0])+int(arr[1]))
-    b = str(int(arr[2])+int(arr[3]))
-    c = str(int(arr[4])+int(arr[5]))
-    d = str(int(arr[6])+int(arr[7]))
-    e = str(int(arr[8])+int(arr[9]))
-    f = str(int(arr[10])+int(arr[11]))
-    g = str(int(arr[12])+int(arr[13]))
-    added = a+b+c+d+e+f+g
-    print (added)
-    print(len(str(added)))
-    arr = se(str(added),1)
-    arr[1] = arr[1].replace("3","0").replace("4","1")
-    arr[3] = arr[3].replace("3","0").replace("4","1")
-    arr[5] = arr[5].replace("3","0").replace("4","1")
-    arr[7]= arr[7].replace("3","0").replace("4","1")
-    arr[9] = arr[9].replace("3","0").replace("4","1")
-    arr[11] = arr[11].replace("3","0").replace("4","1")
-    arr[13] = arr[13].replace("3","0").replace("4","1")
-    a = str(int(arr[0])+int(arr[1]))
-    b = str(int(arr[2])+int(arr[3]))
-    c = str(int(arr[4])+int(arr[5]))
-    d = str(int(arr[6])+int(arr[7]))
-    e = str(int(arr[8])+int(arr[9]))
-    f = str(int(arr[10])+int(arr[11]))
-    g = str(int(arr[12])+int(arr[13]))
-    if len(a)>1:
-        a = a.replace("10","a")
-    if len(b)>1:
-       b = b.replace("10","a")
-    if len(c)>1:
-        c = c.replace("10","a")
-    if len (d)>1:
-        d =d.replace("10","a")
-    if len(e)>1:
-        e = e.replace("10","a")
-    if len(f)>1:
-        f = f.replace("10","a")
-    if len(g)>1:
-        g = g.replace("10","a")
-    added = a+b+c+d+e+f+g
-    print (added)
-    
-    """
+    if int(len(string)) % 2 == 0:
+        arr = se(string,int(len(string)/2))
+    else:
+        arr = se(string,1)
+    finarr = []
+    tarr = []
+    farr = []
+    for i in range(1,len(arr),2):
+        if int(arr[i]) % 2 != 0:
+            print(arr[i])
+            farr = se(arr[i],1)
+            print(farr)
+            for j in range(0, len(farr),1):
+                farr[j] = farr[j].replace("a","x").replace("9","y").replace("8","99").replace("7","88").replace("6","77").replace("5","66").replace("4","55").replace("3","44").replace("2","33").replace("1","22").replace("0","11")
+                farr[j] = farr[j].replace("x","121").replace("y","110")
+            print(arr[i])
+            arr[i] = "".join(farr)
+            print("idhuigfdygafjsgfasgfhkhfjhghagfkashgkafjgsk")
+            print(arr[i])
+            tarr = se(arr[i],2)
+            print(tarr)
+    for i in range(0,len(arr)-1,2):
+        if len(arr) == 2:
+            ca = arr[0]
+            ca = se(ca,1)
+            cb = "".join(tarr)
+            cb = se(cb,2)
+            print(ca)
+            print("ca")
+            print("cb")
+            print(cb)
+        for i in range(0,len(ca),1):
+            
+            a = int(ca[i]) + int(cb[i])
+            if int(a) >=0 and int(a) < 10:
+                b = "0"
+                c = str(a)
+                a = b+c
+            if int(a) > 10:
+                print(a)
+                print 
+                a = charrarr[int(a)]
+            finarr.append(str(a))
+            print(finarr)
+            print(len(finarr))
+        
+       
+    with open("compressed.txt","w",encoding="utf-8") as tf:
+        tf.write("".join(finarr))
+        #You may notice, that the final file size is either larger, or exactly the same as input file. We will need to compress it one more time. But this requires math that i dont feel like doing yet 
+        #Feel free to try your hand at the next iteration!
+        
